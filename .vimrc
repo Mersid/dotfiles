f72f2f1df8febf532def5299880b80da765f8c60
Mersid <mersid_email@yahoo.com>
2022-08-02 19:30:51 -0700

    Initial commit

---
+++ .vimrc
@@ -0,0 +1,6 @@
+set number
+set tabstop=4
+set backspace=indent,eol,start
+set wildmenu
+syntax on
+
---
+++ init.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+# ------------------------------------------------------ I N I T ------------------------------------------------------
+# Ensure script is run as root
+if [ "$EUID" -ne 0 ]
+then
+	echo "This script must be run as root!"
+	exit
+fi
+
+# ------------------------------------------------- F U N C T I O N S -------------------------------------------------
+# Prompt the user for a yes or no answer
+# $1: The string to display to the user
+# $2: 0 for default no, other for default yes
+# $?: 0 if no, 1 if yes
+function prompt {
+	read -p "$1" yn
+	case $yn in
+		[Yy]* ) 
+			return 1;;
+		[Nn]* ) 
+			return 0;;
+		* )
+			if [ "$2" -eq 0 ]
+			then
+				return 0
+			else
+				return 1
+			fi;;
+	esac
+	
+}
+
+# --------------------------------------------------- P R O M P T S ---------------------------------------------------
+
+prompt "Install compiler tools (make, cmake, g++; required for btop)? [Y/n] " 1
+installCompilerTools=$?
+
+# Install btop prompt is shown only if compiler tools will be installed
+cloneBtop=0
+if [ "$installCompilerTools" -eq 1 ]
+then
+	prompt "Install btop? [Y/n]" 1
+	cloneBtop=$?
+fi
+
+prompt "Install nala? [Y/n] " 1
+installNala=$?
+
+prompt "Configuration complete. Ready to install. Proceed? [Y/n] " 1
+if [ "$?" -eq 0 ]
+then
+	echo "Exiting"
+	exit
+fi
+
+# ------------------------------------------------ R U N   S C R I P T ------------------------------------------------
+
+# Update apt repositories
+apt update
+apt full-upgrade
+
+# Install required/requested packages
+apt install -y wget git
+pip3 install mackup
+
+# Bootstrap and run mackup
+
+
+# Install optional dependencies
+
+if [ "$installCompilerTools" -eq 1 ]
+then
+	apt install -y make cmake g++
+fi
+
+if [ "$cloneBtop" -eq 1 ]
+then
+	git clone "github.com/aristocratos/btop"
+fi
+
+if [ "$installNala" -eq 1 ]
+then
+	pip3 install nala
+fi
+
