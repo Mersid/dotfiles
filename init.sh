1cd0c53cd03eda669951155c0e4190354ae10e7f
Mersid <mersid_email@yahoo.com>
2026-08-27 23:55:03 -0700

    Fix shellcheck warning.

--- init.sh
+++ init.sh
@@ -118,7 +118,8 @@ function install_rust() {
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
     # Add cargo env, otherwise it won't work until shell restart
-    . "$HOME/.cargo/env" # shellcheck disable=SC1091
+    # shellcheck disable=SC1091
+    . "$HOME/.cargo/env"
 }
 
 function install_zoxide() {
